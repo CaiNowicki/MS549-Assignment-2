cfac1a3ebd0be128446f74e39e29ebe20b85cc5d
Cai Nowicki <cai.nowicki@gmail.com>
2024-11-10 17:44:42 -0500

    wrote main.cpp file

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "functions.h"
+
+using namespace std;
+
+int main() {
+	srand(time(0)); //randomizer seed
+
+	//create a maze of specified size
+	int width;
+	int height;
+	cout << "Please specify dimensions of your maze: ";
+	cout << "Width: ";
+	cin >> width;
+	cout << "Height: ";
+	cin >> height;
+
+	Maze maze(width, height);
+	//carve maze starting from (0,0)
+	maze.carveMaze(0, 0);
+
+	//print the maze
+	maze.printMaze();
+
+	return 0;
+}
